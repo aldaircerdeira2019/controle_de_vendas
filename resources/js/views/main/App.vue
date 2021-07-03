1df2dc37e51728028271a303947c2bc739d2e1e4
aldaircerdeira2019 <aldaircerdeira10@hotmail.com>
2021-07-03 12:52:32 -0400

    configurando o VueJs

---
+++ resources/js/views/main/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <main class="py-4">
+      <router-view />
+    </main>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SpaController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::redirect('/', '/spa');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/spa{any}', [SpaController::class, 'index'])->where('any', '.*');
