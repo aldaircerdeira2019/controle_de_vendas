31f01a016ded35f9e6b758cb32fee2719f0d5e85
aldaircerdeira2019 <aldaircerdeira10@hotmail.com>
2021-07-03 12:25:44 -0400

    backend - class auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,38 +3,35 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
+
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+    public function login(Request $request){
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+        $user =  !!User::where('email', $request['email'])->get()->count();
+        if(!$user){
+            throw ValidationException::withMessages([
+                'email' =>['Email não cadastrado.']
+            ]);
+        }
+         if (Auth::attempt($request->only('email', 'password'))){
+            return response()->json(Auth::user(), 200);
+        }
+        throw ValidationException::withMessages([
+            'password' =>['Senha incorreta.']
+        ]);
+    }
 
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function logout()
     {
-        $this->middleware('guest')->except('logout');
+        Auth::logout();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\{ RegisterController, LoginController };
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register' , [RegisterController::class, 'register']);
+Route::post('login' , [LoginController::class, 'login']);
+Route::post('logout' , [LoginController::class, 'logout']);
